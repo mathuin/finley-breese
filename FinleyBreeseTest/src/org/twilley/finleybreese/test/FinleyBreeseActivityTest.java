796a39e9502403e8ba7fb114597de3db11f76f6c
Jack Twilley <mathuin@gmail.com>
2013-08-05 19:46:34 -0700

    Added FinleyBreese and FinleyBreeseTest projects.
    They're just "hello world" for now, but stay tuned...

---
+++ FinleyBreeseTest/src/org/twilley/finleybreese/test/FinleyBreeseActivityTest.java
@@ -0,0 +1,14 @@
+package org.twilley.finleybreese.test;
+
+import org.twilley.finleybreese.FinleyBreeseActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class FinleyBreeseActivityTest extends
+		ActivityInstrumentationTestCase2<FinleyBreeseActivity> {
+	
+	public FinleyBreeseActivityTest() {
+		super("org.twilley.finleybreese", FinleyBreeseActivity.class);
+	}
+
+}
