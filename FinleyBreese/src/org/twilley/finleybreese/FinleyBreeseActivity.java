65bf9cb5ded0c3df18e990d885b821f979437deb
Jack Twilley <mathuin@gmail.com>
2013-08-08 14:04:20 -0700

    Moved Morse code into its own class.
    A new class was created for the Morse code.  It now includes on-the-fly
    tone generation as well as a much cleaner public interface.  Need more
    tests.

--- FinleyBreese/src/org/twilley/finleybreese/FinleyBreeseActivity.java
+++ FinleyBreese/src/org/twilley/finleybreese/FinleyBreeseActivity.java
@@ -1,5 +1,6 @@
 package org.twilley.finleybreese;
 
+import org.twilley.finleybreese.Morse;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -26,28 +27,10 @@ public class FinleyBreeseActivity extends Activity {
     private static final String TAG = "FinleyBreese";
 	private static boolean logging = true;
 	
-	// resource access
-	Resources res;
-	// resource values
-	int bitwidth;
-	int samplerate;
-	int channels;
-	int pitch;
-	int cspeed;
-	int speed;
-	int lenies;
-	int lenics;
-	int leniws;
 	// hashes
 	private HashMap<String, String> rthash = new HashMap<String, String>();
-	private HashMap<Character, String> morse = new HashMap<Character, String>();
-	// waveform data
-	private byte[] dit;
-	private byte[] dah;
-	private byte[] ies;
-	private byte[] ics;
-	private byte[] iws;
-
+	private Morse myMorse;
+	
 	/** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +60,19 @@ public class FinleyBreeseActivity extends Activity {
     			Log.w(TAG, "no ringtones found");
     	}
     	
+    	// resource access
+    	Resources res;
+    	// resource values
+    	int bitwidth;
+    	int samplerate;
+    	int channels;
+    	int pitch;
+    	int cspeed;
+    	int speed;
+    	int lenies;
+    	int lenics;
+    	int leniws;
+
     	// deal with resources
     	res = getResources();
     	int[] statsarr = res.getIntArray(R.array.stats);
@@ -90,11 +86,9 @@ public class FinleyBreeseActivity extends Activity {
     	lenics = statsarr[7];
     	leniws = statsarr[8];
     	
-    	// Populate morse hashmap
-    	morseSetup();
-    	
-    	// Construct lists
-    	listSetup();
+    	// Build Morse object
+    	// myMorse = new Morse(pitch, cpseed, speed, bitwidth, samplerate, channels);
+    	myMorse = new Morse();
     }
 
     /** Called when the activity is becoming visible to the user. */
@@ -165,14 +159,10 @@ public class FinleyBreeseActivity extends Activity {
     			} else {
     				if (logging == true)
     					Log.d(TAG, "ringtone does not exist, create one");
-    				// generate the List<Byte> representing the waveform
-    				String rtcode = tomorse(rtstring);
-    				// TODO: save as .ogg/.wav/.mp3/*something*
-    				// JMT - stolen from ringdroid
     				File rtfile = new File(rtpath, rtstring + ".wav");
     				// TODO: identify it somehow as as a ringtone, of course!
     				try {
-						writeFile(rtfile, rtcode);
+        				myMorse.createFile(rtfile, rtstring);
 					} catch (IOException e) {
 	    				Log.e(TAG, " exiting before corrupting hash or assigning ringtone! ", e);
 	    				continue;
@@ -277,292 +267,5 @@ public class FinleyBreeseActivity extends Activity {
 		}
 	
 		return rtpath;
-    }
-    
-    /** Constructs the dit, dah, and space elements. */
-    protected void listSetup() {
-    	if (logging == true)
-    		Log.v(TAG, "entered listSetup");
-    	
-    	int bytecounter;
-    	int index;
-
-    	int[] resdit = res.getIntArray(R.array.dit);
-    	dit = new byte[resdit.length * 2];
-    	bytecounter = 0;
-    	for (int i: resdit) { 
-    		// Log.v(TAG, "value is " + i);
-    		dit[bytecounter++] = (byte)(i % 0xff);
-    		dit[bytecounter++] = (byte)((i >> 8) & 0xff);
-    	}
-    	if (logging == true)
-			Log.v(TAG, " - dit is " + dit.length + " elements long");
-		
-    	int[] resdah = res.getIntArray(R.array.dah);
-    	dah = new byte[resdah.length * 2];
-    	bytecounter = 0;
-    	for (int i: resdah) {
-    		dah[bytecounter++] = (byte)(i % 0xff);
-    		dah[bytecounter++] = (byte)((i >> 8) & 0xff);
-    	}
-    	if (logging == true)
-			Log.v(TAG, " - dah is " + dah.length + " elements long");
-		
-    	ies = new byte[lenies * 2];
-    	bytecounter = 0;
-    	for (index = 0; index < lenies; index++) {
-    		ies[bytecounter++] = (byte)(0);
-    		ies[bytecounter++] = (byte)(0);
-    	}
-		if (logging == true)
-			Log.v(TAG, " - ies is " + ies.length + " elements long");
- 
-    	ics = new byte[lenics * 2];
-    	bytecounter = 0;
-    	for (index = 0; index < lenics; index++) {
-    		ics[bytecounter++] = (byte)(0);
-    		ics[bytecounter++] = (byte)(0);
-    	}
-		if (logging == true)
-			Log.v(TAG, " - ics is " + ics.length + " elements long");
-
-    	iws = new byte[leniws * 2];
-    	bytecounter = 0;
-    	for (index = 0; index < leniws; index++) {
-    		iws[bytecounter++] = (byte)(0);
-    		iws[bytecounter++] = (byte)(0);
-    	}
-		if (logging == true)
-			Log.v(TAG, " - iws is " + iws.length + " elements long");
-    }
-    
-    /** Constructs the morse hashmap. */
-    protected void morseSetup() {
-    	if (logging == true)
-    		Log.v(TAG, "entered morseSetup");
-    	
-		// construct hashmap
-		morse.put('a', ".-");
-		morse.put('b', "-...");
-		morse.put('c', "-.-."); 
-		morse.put('d', "-..");
-		morse.put('e', ".");
-		morse.put('f', "..-.");
-		morse.put('g', "--.");
-		morse.put('h', "....");
-		morse.put('i', "..");
-		morse.put('j', ".---");
-		morse.put('k', "-.-");
-		morse.put('l', ".-..");
-		morse.put('m', "--");
-		morse.put('n', "-.");
-		morse.put('o', "---");
-		morse.put('p', ".--.");
-		morse.put('q', "--.-");
-		morse.put('r', ".-.");
-		morse.put('s', "...");
-		morse.put('t', "-");
-		morse.put('u', "..-");
-		morse.put('v', "...-");
-		morse.put('w', ".--");
-		morse.put('x', "-..-");
-		morse.put('y', "-.--");
-		morse.put('z', "--..");
-		morse.put('0', "-----");
-		morse.put('1', ".----");
-		morse.put('2', "..---");
-		morse.put('3', "...--");
-		morse.put('4', "....-");
-		morse.put('5', ".....");
-		morse.put('6', "-....");
-		morse.put('7', "--...");
-		morse.put('8', "---..");
-		morse.put('9', "----.");
-		morse.put('.', ".-.-.-");
-		morse.put(',', "--..--");
-		morse.put('?', "..--..");
-		morse.put(':', "---...");
-		morse.put(';', "-.-.-.");
-		morse.put('-', "-....-");
-		morse.put('/', "-..-.");
-		morse.put('\"', ".-..-.");
-		morse.put('+', ".-.-.");
-		morse.put('|', ".-...");
-		morse.put('>', "-.--.");
-		morse.put('~', "...-.-");
-		morse.put('=', "-...-");
-		morse.put('@', ".--.-.");
-    }
-    
-    /** Builds the list of bytes from the text. */
-    protected String tomorse(String input) {
-    	if (logging == true)
-    		Log.v(TAG, "entered tomorse");
-    	
-		String output = "";
-		String letter;
-		int lastel;
-		int spaceflag = 1;
-		CharacterIterator it = new StringCharacterIterator(input);
-		CharacterIterator it2;
-	    
-	    for (char ch = it.first(); ch != CharacterIterator.DONE; ch = it.next()) {
-	    	Log.v(TAG, " - character is " + ch);
-	        if (Character.isWhitespace(ch) && spaceflag == 0) {
-	        	// append iws to output
-	        	if (logging == true)
-	        		Log.v(TAG, "append iws to output");
-	        	output += "iws ";
-	        	spaceflag = 1;
-	        } else {
-	        	if (morse.containsKey(ch)) {
-	        		letter = morse.get(ch);
-	        		if (logging == true)
-	        			Log.v(TAG, " - morse contains key " + ch + ", value is " + letter);
-	        		if (spaceflag == 0) {
-	        			// append ics to output
-	        			if (logging == true)
-	        				Log.v(TAG, "append ics to output");
-	        			output += "ics ";
-	        		} else
-	        			spaceflag = 0;
-	        		lastel = letter.length() - 1;
-	        		it2 = new StringCharacterIterator(letter);
-	        		for (char el = it2.first(); el != CharacterIterator.DONE; el = it2.next()) {
-	        			if (logging == true)
-	        				Log.v(TAG, " - morse character is " + el);
-	        			if (el == '.') {
-	        				// append dit to output
-	        	        	if (logging == true)
-	        	        		Log.v(TAG, "append dit to output");
-	        				output += "dit ";
-	        			} else {
-	        				// append dah to output
-	        	        	if (logging == true)
-	        	        		Log.v(TAG, "append dah to output");
-	        				output += "dah ";
-	        			}
-	        			if (logging == true)
-	        				Log.v(TAG, "it2.getIndex() is " + it2.getIndex() + ", lastel is " + lastel);
-	        			if (it2.getIndex() != lastel) {
-	        				// append ies to output
-	        	        	if (logging == true)
-	        	        		Log.v(TAG, "append ies to output");
-		        			output += "ies ";
-	        			} 
-	        		}
-	        	}
-	        }
-	    }
-		return output;
-    }
-    
-    /** Write ringtone to a file. */
-    protected void writeFile(File file, String code) 
-    throws java.io.IOException {
-    	if (logging == true)
-    		Log.v(TAG, "entered writeFile");
-    	
-    	// split the code string up into elements
-    	String[] splitcode = code.split(" ");
-    	if (logging == true)
-    		Log.v(TAG, "code split into " + splitcode.length + " elements");
-    	
-    	// JMT: stolen from ringdroid
-    	// create the file
-    	file.createNewFile();
-		FileOutputStream out = new FileOutputStream(file);
-		
-		// generate length values
-		long totalAudioLen = 0;
-		for (String s : splitcode) {
-			if (s.contentEquals("dit")) {
-				totalAudioLen += dit.length;
-			} else if (s.contentEquals("dah")) {
-				totalAudioLen += dah.length;
-			} else if (s.contentEquals("ies")) {
-				totalAudioLen += ies.length;
-			} else if (s.contentEquals("ics")) {
-				totalAudioLen += ics.length;
-			} else if (s.contentEquals("iws")) {
-				totalAudioLen += iws.length;
-			} else{
-				Log.e(TAG, "bad value in splitcode: " + s);
-			}
-		}
-		if (logging == true)
-			Log.v(TAG, "calculated total audio length: " + totalAudioLen);
-		
-		// previous answer given in bytes, need samples
-		totalAudioLen /= 2;
-    	long totalDataLen = totalAudioLen + 36;
-    	long longSampleRate = samplerate;
-    	long byteRate = samplerate * 2 * channels;
-    	
-    	// write the header
-        byte[] header = new byte[44];
-        header[0] = 'R';  // RIFF/WAVE header
-        header[1] = 'I';
-        header[2] = 'F';
-        header[3] = 'F';
-        header[4] = (byte) (totalDataLen & 0xff);
-        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
-        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
-        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
-        header[8] = 'W';
-        header[9] = 'A';
-        header[10] = 'V';
-        header[11] = 'E';
-        header[12] = 'f';  // 'fmt ' chunk
-        header[13] = 'm';
-        header[14] = 't';
-        header[15] = ' ';
-        header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
-        header[17] = 0;
-        header[18] = 0;
-        header[19] = 0;
-        header[20] = 1;  // format = 1
-        header[21] = 0;
-        header[22] = (byte) channels;
-        header[23] = 0;
-        header[24] = (byte) (longSampleRate & 0xff);
-        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
-        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
-        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
-        header[28] = (byte) (byteRate & 0xff);
-        header[29] = (byte) ((byteRate >> 8) & 0xff);
-        header[30] = (byte) ((byteRate >> 16) & 0xff);
-        header[31] = (byte) ((byteRate >> 24) & 0xff);
-        header[32] = (byte) (2 * channels);  // block align
-        header[33] = 0;
-        header[34] = 16;  // bits per sample
-        header[35] = 0;
-        header[36] = 'd';
-        header[37] = 'a';
-        header[38] = 't';
-        header[39] = 'a';
-        header[40] = (byte) (totalAudioLen & 0xff);
-        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
-        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
-        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
-        out.write(header, 0, 44);
-        
-    	// write the data
-		for (String s : splitcode) {
-			if (s.contentEquals("dit")) {
-				out.write(dit);
-			} else if (s.contentEquals("dah")) {
-				out.write(dah);
-			} else if (s.contentEquals("ies")) {
-				out.write(ies);
-			} else if (s.contentEquals("ics")) {
-				out.write(ics);
-			} else if (s.contentEquals("iws")) {
-				out.write(iws);
-			} else{
-				Log.e(TAG, "bad value in splitcode: " + s);
-			}
-		}
-        out.close();
     }
 }
---
+++ FinleyBreese/src/org/twilley/finleybreese/Morse.java
@@ -0,0 +1,307 @@
+package org.twilley.finleybreese;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.Math;
+
+/**
+ * This class combines some audio file wizardry with Morse code skills.
+ * @author jmt
+ *
+ */
+
+public class Morse {
+	// class implementation comment
+	
+	// public interface
+	// public constants, class and instance variables
+	// public constructors
+	public Morse(int startPitch, int startCspeed, int startSpeed, int startBitwidth, int startSamplerate, int startChannels) {
+		pitch = startPitch;
+		if (startCspeed < startSpeed)
+			startCspeed = startSpeed;
+		cspeed = startCspeed;
+		speed = startSpeed;
+		if (bitwidth % 8 != 0)
+			bitwidth = (int) (8 * Math.ceil(bitwidth/8.0));
+		bitwidth = startBitwidth;
+		samplerate = startSamplerate;
+		channels = startChannels;
+
+		elements = buildElements();
+	}
+	
+	public Morse(int startPitch, int startCspeed, int startSpeed) {
+		this(startPitch, startCspeed, startSpeed, DEFAULT_BITWIDTH, DEFAULT_SAMPLERATE, DEFAULT_CHANNELS);
+	}
+
+	public Morse() {
+		this(DEFAULT_PITCH, DEFAULT_CSPEED, DEFAULT_SPEED, DEFAULT_BITWIDTH, DEFAULT_SAMPLERATE, DEFAULT_CHANNELS);
+	}
+	
+	// public methods
+	public void createFile(File file, String input) throws IOException {
+		ArrayList<String> bits = makeSyllables(input);
+		byte[] samples = buildSamples(bits);
+		writeFile(file, samples);
+	}
+	
+	
+	// public nested classes and interfaces
+	
+	// package interface
+	// package constants, class and instance variables
+	// package constructors
+	// package methods
+	// package nested classes and interfaces
+	
+	// protected
+	
+	// private constants
+	// default values for instance variables
+	private static final int DEFAULT_PITCH = 880;
+	private static final int DEFAULT_CSPEED = 20;
+	private static final int DEFAULT_SPEED = 13;
+	private static final int DEFAULT_BITWIDTH = 32;
+	private static final int DEFAULT_SAMPLERATE = 44100;
+	private static final int DEFAULT_CHANNELS = 1;
+	
+	// the length of the word 'paris' in dits
+	private static final int PARIS_LENGTH = 50;
+	
+	// what fraction of full range is used for the tone
+	private static final double MAX_AMPLITUDE = 0.85;
+	// how many seconds from zero to max amplitude
+	private static final double SLOPE = 0.005;
+	
+	// private class variable
+	private static final HashMap<Character, String> chardict = initializeChardict();
+		
+	// private instance variables
+	private int pitch;
+	private int cspeed;
+	private int speed;
+	private int bitwidth;
+	private int samplerate;
+	private int channels;	
+	private HashMap<String, byte[]> elements;
+
+	// build byte arrays for different elements!
+	private HashMap<String, byte[]> buildElements() {
+		HashMap<String, byte[]> myelements = new HashMap<String, byte[]>();
+		
+		int lendit;
+		int farndit;
+		
+		// calculate lengths
+		lendit = samplerate * 60 / (PARIS_LENGTH * cspeed);
+		
+		if (cspeed < speed) {
+			double ta = 60 / (double) speed - 37.2 / (double) cspeed;
+			farndit = (int) (samplerate * ta) / 19;
+		} else {
+			farndit = samplerate * 60 / (PARIS_LENGTH * speed);
+		}
+		
+		// create and populate arrays
+		myelements.put("dit", tone(lendit, pitch));
+		myelements.put("dah", tone(lendit * 3, pitch));
+		myelements.put("ies", tone(lendit, 0));
+		myelements.put("ics", tone(farndit * 3, 0));
+		myelements.put("iws", tone(farndit * 7, 0));
+		
+		return myelements;
+	}
+	
+	// initializes class variable
+	private static HashMap<Character, String> initializeChardict() {
+		HashMap<Character, String> mydict = new HashMap<Character, String>();
+		
+		mydict.put('a', ".-");
+		mydict.put('b', "-...");
+		mydict.put('c', "-.-."); 
+		mydict.put('d', "-..");
+		mydict.put('e', ".");
+		mydict.put('f', "..-.");
+		mydict.put('g', "--.");
+		mydict.put('h', "....");
+		mydict.put('i', "..");
+		mydict.put('j', ".---");
+		mydict.put('k', "-.-");
+		mydict.put('l', ".-..");
+		mydict.put('m', "--");
+		mydict.put('n', "-.");
+		mydict.put('o', "---");
+		mydict.put('p', ".--.");
+		mydict.put('q', "--.-");
+		mydict.put('r', ".-.");
+		mydict.put('s', "...");
+		mydict.put('t', "-");
+		mydict.put('u', "..-");
+		mydict.put('v', "...-");
+		mydict.put('w', ".--");
+		mydict.put('x', "-..-");
+		mydict.put('y', "-.--");
+		mydict.put('z', "--..");
+		mydict.put('0', "-----");
+		mydict.put('1', ".----");
+		mydict.put('2', "..---");
+		mydict.put('3', "...--");
+		mydict.put('4', "....-");
+		mydict.put('5', ".....");
+		mydict.put('6', "-....");
+		mydict.put('7', "--...");
+		mydict.put('8', "---..");
+		mydict.put('9', "----.");
+		mydict.put('.', ".-.-.-");
+		mydict.put(',', "--..--");
+		mydict.put('?', "..--..");
+		mydict.put(':', "---...");
+		mydict.put(';', "-.-.-.");
+		mydict.put('-', "-....-");
+		mydict.put('/', "-..-.");
+		mydict.put('\"', ".-..-.");
+		mydict.put('+', ".-.-.");
+		mydict.put('|', ".-...");
+		mydict.put('>', "-.--.");
+		mydict.put('~', "...-.-");
+		mydict.put('=', "-...-");
+		mydict.put('@', ".--.-.");
+		
+		return mydict;
+	}
+	
+	// generate tone based on frequency and length
+	private byte[] tone(int length, int frequency) {
+		int samplewidth = bitwidth/8;
+		byte[] mytone = new byte[length * channels * samplewidth];
+		int grade = (int) (SLOPE * MAX_AMPLITUDE * samplerate);
+		int last = length - grade;
+		double maxvalue = MAX_AMPLITUDE * Math.pow(2, bitwidth-1); 
+
+		for (int index = 0; index < length; index++) {
+			for (int c = 0; c < channels; c++) {
+				double elem = maxvalue * Math.sin(frequency * Math.PI * 2 * index / samplerate);
+				if (index < grade)
+					elem *= (index / grade);
+				else if (index > last);
+					elem *= ((length - index) / grade);
+				for (int s = 0; s < samplewidth; s++)
+					mytone[(channels * index + c) * samplewidth + s] = (byte) (((int) elem >> (8 * s)) & 0xff);
+			}
+		}
+		return mytone;
+	}
+	
+	private ArrayList<String> makeSyllables(String input) {
+		ArrayList<String> retval = new ArrayList<String>();
+		String letter;
+		boolean spaceflag = true;
+		
+		for (char c: input.toCharArray()) {
+			if (Character.isWhitespace(c) && !spaceflag) {
+				retval.add("iws");
+				spaceflag = true;
+			} else {
+				if (chardict.containsKey(c)) {
+					letter = chardict.get(c);
+					if (!spaceflag)
+						retval.add("ics");
+					else
+						spaceflag = false;
+					for (char el: letter.toCharArray()) {
+						if (el == '.')
+							retval.add("dit");
+						else
+							retval.add("dah");
+						retval.add("ies");
+					}
+					retval.remove(retval.lastIndexOf("ies"));
+				}
+			}
+		}
+		return retval;
+	}
+	
+	// construct sample array from syllables
+	private byte[] buildSamples(ArrayList<String> bits) throws IOException {
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		
+		for (String s: bits) {
+			output.write(elements.get(s));
+		}
+		
+		return output.toByteArray();
+	}
+	
+	// construct WAV file 
+	private void writeFile(File file, byte[] samples) throws IOException {
+		file.createNewFile();
+		FileOutputStream out = new FileOutputStream(file);
+				
+		byte[] header = new byte[44];
+        int blockalign = channels * bitwidth/8;
+        int byterate = samplerate * blockalign;
+        
+        // may be worth writing int->4byte, string->byte, etc
+
+        int audiolen = samples.length;
+        // 36 is everything but the data, 'RIFF' and 'WAVE'.
+        int datalen = audiolen + 36;
+        
+        header[0] = 'R';  // RIFF/WAVE header
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (datalen & 0xff);
+        header[5] = (byte) ((datalen >> 8) & 0xff);
+        header[6] = (byte) ((datalen >> 16) & 0xff);
+        header[7] = (byte) ((datalen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f';  // 'fmt ' chunk
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1;  // format = 1
+        header[21] = 0;
+        header[22] = (byte) channels;
+        header[23] = 0;
+        header[24] = (byte) (samplerate & 0xff);
+        header[25] = (byte) ((samplerate >> 8) & 0xff);
+        header[26] = (byte) ((samplerate >> 16) & 0xff);
+        header[27] = (byte) ((samplerate >> 24) & 0xff);
+        header[28] = (byte) (byterate & 0xff);
+        header[29] = (byte) ((byterate >> 8) & 0xff);
+        header[30] = (byte) ((byterate >> 16) & 0xff);
+        header[31] = (byte) ((byterate >> 24) & 0xff);
+        header[32] = (byte) blockalign;  // block align
+        header[33] = 0;
+        header[34] = (byte) bitwidth;  // bits per sample
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (audiolen & 0xff);
+        header[41] = (byte) ((audiolen >> 8) & 0xff);
+        header[42] = (byte) ((audiolen >> 16) & 0xff);
+        header[43] = (byte) ((audiolen >> 24) & 0xff);
+        out.write(header);
+        
+        // now write the samples
+        out.write(samples); 
+        
+        out.close();
+	}
+}
